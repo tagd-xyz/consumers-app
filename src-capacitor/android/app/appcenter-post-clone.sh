2ccd59dcac23b1a2c22554389b56ef99867b0ec4
Juan Sierra <juan@totally.group>
2023-03-27 21:06:56 +0200

    AppCenter builds
    AppCenter builds
    
    
    AppCenter builds
    
    
    AppCenter builds
    
    
    AppCenter builds
    
    
    AppCenter builds

--- src-capacitor/android/app/appcenter-post-clone.sh
+++ src-capacitor/android/app/appcenter-post-clone.sh
@@ -1,14 +1,40 @@
 #!/usr/bin/env bash
 
-# Example: Change bundle name of an iOS app for non-production
-# if [ "$APPCENTER_BRANCH" != "main" ];
-# then
-#     plutil -replace CFBundleDisplayName -string "\$(PRODUCT_NAME) Beta" $APPCENTER_SOURCE_DIRECTORY/MyApp/Info.plist
-# fi
+echo "---[ POST-CLONE ]--------------------------------------------------------------"
 
+# fail if any command fails
+set -e
+# debug log
+set -x
 
-npx cap update android
-# or remove capacitor-cordova-android-plugins from .gitignore
+# Required nodeJS version
+NODE_VERSION=16.16.0
 
-npx cap copy android
-# or remove app/src/main/assets/public from .gitignore
+# workaround to override the v8 alias
+npm config delete prefix
+. ~/.bashrc
+nvm install "$NODE_VERSION"
+nvm use ${NODE_VERSION}
+
+# install quasar
+yarn global add @quasar/cli
+
+# go to root of project
+cd ../../..
+
+# install dependencies
+yarn install
+
+# go to src-capacitor and install dependencies there
+pwd
+ls -la
+cd src-capacitor
+yarn install
+
+# run optimized production build
+quasar build -m capacitor -T android --skip-pkg
+
+# copy the web assets to the native projects and updates the native plugins and dependencies based in package.json
+npx cap sync android
+
+echo "---[ POST-CLONE ]--------------------------------------------------------------"
